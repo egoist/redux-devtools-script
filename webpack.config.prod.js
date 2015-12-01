265aecbb07d93a1229fcd31b8082ae3166b19325
egoist <aprilorange.net@icloud.com>
2015-12-01 21:19:25 +0800

    add build script

---
+++ webpack.config.prod.js
@@ -0,0 +1,16 @@
+var webpack = require('webpack')
+var config = require('./webpack.config')
+config.plugins = [
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compressor: {
+      warnings: false
+    }
+  })
+]
+module.exports = config
